75322d1d47dceb9cf45b125d9353be34839ffe52
Shashank Soni <shashanksoni815@gmail.com>
2026-04-25 11:59:37 +0530

    prime

--- Practice/test.java
+++ Practice/test.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 public class test {
     public static void isEven(int num) {
         if(num % 2 == 0) {
-            System.out.print(": is Even");
+            System.out.println(": is Even");
         } else {
-            System.out.print(": is Odd");
+            System.out.println(": is Odd");
         }
     }
 
+    public static boolean isPrime(int n) {
+        for(int i = 2; i < n; i++) {
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String arge[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         isEven(n);
-            System.out.print("Hello World");
+        System.out.println("is prime: " + isPrime(n));
+
+            System.out.println("Hello World");
         sc.close();
 
     }
