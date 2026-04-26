e3a61900e9d121fdf895023f6d757444779f7d6d
Shashank Soni <shashanksoni815@gmail.com>
2026-04-26 13:52:36 +0530

    Max Subarrayt

--- Practice/Arr.java
+++ Practice/Arr.java
@@ -10,12 +10,68 @@ public class Arr {
         return -1;
     }
     
+    public static void sum(int num[], int key) {
+        for(int i = 0; i < num.length; i++) {
+            for(int j = i+1; j < num.length; j++) {
+                if(num[i] + num[j] == key) {
+                    System.out.println("The pair is: " + num[i] + " and " + num[j]);
+                }
+            }
+        }
+        // System.out.println("No pair found");
+    }
     
-    public static void main(String[] args) {
-        int num[] = {1, 3, 5, 7, 8, 9};
-        int key = 18;
-        System.out.print(found(num, key));
-    }   
+    public static void sumMaxArr(int arr[]) {
+      int max = Integer.MIN_VALUE;
+      int add = 0;
+        for(int i =0; i< arr.length; i++){
+            for(int j=i; j <= arr.length; j++){
+                add = 0;
+                for(int k=i; k<j; k++){
+                    System.out.print(arr[k] + " ");
+                    add += arr[k];
+                }
+                    System.out.print(" = " + add );
+                    if(add > max){
+                        max = add;
+                    }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.print(max);
         
-         
+    }
+    
+    public static void sumMaxArr2(int arr[]) {
+        int max = Integer.MIN_VALUE;
+        int curr = 0;
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for(int i=1; i< arr.length; i++){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+        
+
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i; j < arr.length; j++){
+                curr = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
+                System.out.println(curr);
+                if(curr > max){
+                    max = curr;
+                }
+            }
+        }
+        System.out.print(max);
+    }
+
+
+    public static void main(String[] args) {
+        int num[] = {1, -2, 6, -1, 3};
+        // int key = 17;
+        // System.out.println(found(num, key));
+        // sum(num, key);
+        sumMaxArr2(num);
+    }    
 }
