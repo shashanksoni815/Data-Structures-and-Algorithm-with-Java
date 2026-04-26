d23fbe6a1ca356bdc97932dcec1142be79904fcd
Shashank Soni <shashanksoni815@gmail.com>
2026-04-26 23:12:10 +0530

    reverse

--- Practice/Arr.java
+++ Practice/Arr.java
@@ -133,15 +133,25 @@ public class Arr {
         System.out.println();
     }
 
+    public static void reverse(int arr[]) {
+        for(int i=0; i<arr.length/2; i++){
+            int temp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i] = temp;
+
+        }
+        printArr(arr);
+    } 
     public static void main(String[] args) {
-        int num[] = {1, -2, 6, -1, 3};
+        int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         // int key = 17;
         // System.out.println(found(num, key));
         // sum(num, key);
-        Kadanes(num);
-        int height[] = {4, 2, 0, 6, 3, 2, 5};
+        // Kadanes(num);
+        // int height[] = {4, 2, 0, 6, 3, 2, 5};
         // int target = 15;
-        trapedWater(height);
-        printArr(height);
+        // trapedWater(height);
+        // printArr(height);
+        reverse(num);
     }    
 }
